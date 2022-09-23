cbce626f04502fabf7b466455b918d68d5e91eed
umasri22 <umasridesetty@gmail.com>
2022-09-23 11:57:46 +0530

    simple-java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Hello{  
     public static void main(String[] args){  
-    System.out.println("This is java app \n by using Google Cloud Platform");  
+    System.out.println("This is java app \n by using Google Cloud Platform 23-sep-2022");  
     }  
     }  
